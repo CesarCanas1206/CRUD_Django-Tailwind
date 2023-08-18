5c3cc57d39fbcea80487e9e5e58decb11d164e6b
kvenus-uj <alexstepan0209@gmail.com>
2023-08-18 02:09:38 -0400

    add toastr in homepage

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.contrib import messages
 # from .models import Account
 
 
 def home(request):
+    messages.success(request, "Welcome!")
     return render(request, 'home.html')
