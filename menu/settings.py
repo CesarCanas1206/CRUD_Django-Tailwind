87dad9aaac83fa91c4ec000e33901bcd4805277d
kvenus-uj <alexstepan0209@gmail.com>
2023-08-23 17:32:47 +0900

    change database from db3 to postgreSQL

--- menu/settings.py
+++ menu/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'menu.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'menudb',
+        'USER': 'postgres',
+        'PASSWORD': 'oskar',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
